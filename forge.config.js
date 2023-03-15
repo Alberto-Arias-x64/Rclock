f6da9ee6f4df2e39d952a3f605389343d8ac4199
x64 <alberto.arias.x64@gmail.com>
2023-03-15 15:38:33 -0500

    full build

---
+++ forge.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  packagerConfig: {},
+  rebuildConfig: {},
+  makers: [
+    {
+      name: '@electron-forge/maker-squirrel',
+      config: {},
+    },
+    {
+      name: '@electron-forge/maker-zip',
+      platforms: ['darwin'],
+    },
+    {
+      name: '@electron-forge/maker-deb',
+      config: {},
+    },
+    {
+      name: '@electron-forge/maker-rpm',
+      config: {},
+    },
+  ],
+};
+// npm run make -- --platform win32
+// npm run make -- --platform linux
+// npm run make -- --platform darwin
